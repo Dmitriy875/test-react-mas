f07fd866b2c788e721bc7fab8f5fba0bf4a478b2
Dmitriy875 <solleks777@gmail.com>
2021-06-29 12:00:58 +0800

    Подъём фильтра.

--- src/users_table/UsersTable.js
+++ src/users_table/UsersTable.js
@@ -1,9 +1,10 @@
 import { Table, TableRow, TableCell, TableBody, TableHead, TableContainer } from '@material-ui/core';
 import React, { Component } from 'react';
-import Filter from './Filter.js';
 import ModalWindow from './ModalWindow.js';
 import { withStyles } from '@material-ui/core/styles';
-import { Grid, Paper } from '@material-ui/core';
+import { Grid, Paper, Container, Typography, Button } from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
 
 export default class UsersTable extends Component {
   constructor(props) {
@@ -16,7 +17,10 @@ export default class UsersTable extends Component {
       modalVisible: false,
       todos: [],
       modalChanged: false,
+      usersOutput: '',
     }
+    this.usersFiltration = this.usersFiltration.bind(this);
+    this.resetFilter = this.resetFilter.bind(this);
   }
 
   componentDidMount() {
@@ -39,7 +43,45 @@ export default class UsersTable extends Component {
     )
   }
 
-  handleRowClick(name, id) {
+  usersFiltration( name ) {
+    let result;
+    if( name ) {
+      this.setState({
+        modalVisible: false
+      })
+      result = this.state.users.filter( user => user.website === name )
+      if( result.length < 1 ) {
+        result = this.state.users.filter( user => user.name === name )
+      }
+    }
+    this.setState({ usersOutput: result })
+  }
+
+  resetFilter() {
+    if( this.state.usersOutput ) {
+      this.setState({ usersOutput: false, input: false })
+    }
+  }
+
+  getInput( name, value ) {
+    if( name ) {
+      let res = value.target.id.match(/nameSelect/)
+        ? 'name'
+        : 'site'
+
+      this.setState({
+        input: name,
+        inputType: res
+      })
+    } else {
+      this.setState({
+        input: '',
+        inputType: '',
+      })
+    }
+  }
+
+  handleRowClick( name, id ) {
     this.setState( state => ({
       name: name,
       id: id,
@@ -73,7 +115,10 @@ export default class UsersTable extends Component {
         },
       },
     }))(TableRow);
-    const {error, isLoaded, users} = this.state;
+    const {error, isLoaded} = this.state;
+    let users = this.state.usersOutput
+      ? this.state.usersOutput
+      : this.state.users
 
     if( error ) {
       return <p>Error {error.message}</p>
@@ -81,12 +126,66 @@ export default class UsersTable extends Component {
       return <p>Loading...</p>
     } else {
       return (
+
         <div className="content-table">
         <Grid container direction="row" justify="center" alignItems="center">
             <Grid item xs={5}>
               <Paper>
-              
-            <Filter users={this.state.users}/>
+
+              <div className="filter">
+                <Container>
+                  <Grid container direction="row"  justify="center" alignItems="center" spacing={5}>
+                      <Grid item xs={12}>
+                        <div>
+                          <Typography variant="h5">
+                            Фильтр
+                          </Typography>
+                          <div style={{ width: 300 }}>
+                            <Autocomplete
+                              id="nameSelect"
+                              freeSolo
+                              onChange={(event, value) => {this.getInput(value, event)}}
+                              disabled={this.state.inputType === 'site' ? true : false}
+                              inputValue={ this.state.input ? this.state.input : '' }
+                              options={this.state.users.map((option) => option.name)}
+                              renderInput={(params) => (
+                                <TextField {...params} label="Enter Username" margin="normal" variant="outlined" />
+                              )}
+                            />
+                            <Autocomplete
+                              freeSolo
+                              id="websiteSelect"
+                              onChange={(event, value) => {this.getInput(value, event)}}
+                              disabled={this.state.inputType === 'name' ? true : false}
+                              inputValue={ this.state.input ? this.state.input : '' }
+                              options={this.state.users.map((option) => option.website)}
+                              renderInput={(params) => (
+                                <TextField
+                                  {...params}
+                                  label="Enter website name"
+                                  margin="normal"
+                                  variant="outlined"
+                                  InputProps={{ ...params.InputProps, type: 'search' }}
+                                />
+                              )}
+                            />
+                          </div>
+                        </div>
+                      </Grid>
+                  </Grid>
+                </Container>
+
+                <Container>
+                  <Grid item xs={12} container justify="flex-end">
+                    <Grid item xs={2} >
+                      <Button variant="outlined" color='secondary' onClick={this.resetFilter}>Сбросить</Button>
+                    </Grid>
+                    <Grid item xs={2}>
+                      <Button variant="outlined" color='primary' onClick={() => {this.usersFiltration(this.state.input)}}>Применить</Button>
+                    </Grid>
+                  </Grid>
+                </Container>
+              </div>
 
             <TableContainer>
               <Table>
